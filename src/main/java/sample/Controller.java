0d267a90a83c3133ac9250122c1e010ae252af31
Oskar <oskarbank@gmail.com>
2017-10-18 19:19:42 +0200

    asd

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.fxml.Initializable;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
@@ -11,5 +12,13 @@ public class Controller implements Initializable{
 
     public void initialize(URL location, ResourceBundle resources) {
         RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Password-App","akademiapremium" );
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity("parameters", httpHeaders);
+
+       ResponseEntity<String> s = restTemplate.exchange(
+                        "http://localhost:8080/rest/reservation", HttpMethod.GET, entity, String.class);
+        System.out.println(s.toString());
     }
 }
